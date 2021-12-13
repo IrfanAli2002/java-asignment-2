9887adee41f85c280aa5b3d9c422a91b7527959e
IrfanAli2002 <94832900+IrfanAli2002@users.noreply.github.com>
2021-12-13 16:58:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,146 @@
+// Chapter 21to25 start
+
+
+// var name = prompt("enter your first name ")
+// var Lastname = prompt("enter your last name ")
+// var fullname = name + Lastname + ("welcome to my website ")
+// alert(fullname)
+
+
+// var pak = ("pakistan")
+
+// for(i=0; i < pak.length; i++){
+    //     var a = pak.slice(i,i + 1)
+    //     if(a == "n")
+    //     console.log("numbers of string are  " + i)
+    // }
+
+    
+
+    // var mob = prompt("enter your favorite mobile model ")
+    // for(i=0; i < mob.length; i++){
+    
+    //    console.log(i)
+    // }
+    
+
+    
+// var b = ("hello world")
+
+// for(i=0; i < b.length; i++){
+//     var a = b.splice(i,i + 1)
+//     if(a == "l")
+//     console.log("numbers of string are  " + i)
+// }
+
+
+// var pak = ("pakistan")
+
+// for(i=0; i < pak.length; i++){
+//     var a = pak.slice(i,i + 1)
+//     if(a == "i")
+//     console.log("numbers of string are  " + i)
+// }
+
+
+
+// var a = ("hyderabad")
+// var b = a.replace("hydera","islama")
+// alert(b)
+
+
+
+// var friend = "Ali and Sami are best friends. They play cricket and football together"
+// var b = friend.replace("and","&")
+// alert(b)
+
+
+
+
+// var a = 476;
+// var b = a.toString()
+// console.log(b)
+
+
+
+// var a = "hello world"
+// var b = a.toUpperCase()
+// alert(b)
+
+
+
+// var a = prompt("Enter your comment")
+// var b = a.toUpperCase()
+// alert(b)
+
+
+// var a = "35.34"
+// var b = parseInt(a)
+// console.log(b)
+
+
+// var a = prompt("Enter username")
+// if(a == "!"){
+//     alert("this is invalid username")
+// }
+
+
+// var a = ["cake","pastry","chips"]   
+
+// for(i = 0; i < a.length; i++){
+//     var b = prompt("what you want")
+//     if(b = "cake" ){
+//         alert("available")
+//     }
+// }
+
+
+
+// Chapter 21to25 end
+
+// Chapter 25to30 start
+
+// var a = 3.54
+// var b = Math.round(a)
+// var c = Math.ceil(a)
+// var d = Math.floor(a)
+// alert(d)
+
+
+
+
+// var a = -3.54
+// var b = Math.round(a)
+// var c = Math.ceil(a)
+// var d = Math.floor(a)
+// alert(d)
+
+
+
+// var a = 1.5;
+// var b = Math.random(a);
+// var c = Math.round(b) 
+
+// if(c == 0){
+//     console.log("head")
+// }else(console.log("tails"))
+
+
+// var a = prompt("enter the secret number")
+// var b = 5;
+
+// if(a == b){
+//     alert("congrats")
+// }else(alert("sorry"))
+
+
+
+
+
+
+
+
+
+
+
+// Chapter 25to30 end
